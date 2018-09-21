320d88fd47bf5bfd3b6cc81f5784f4403ad3030c
Lukas Paul <lukas.paul@gmx.at>
2018-09-21 23:05:40 +0200

    Updated ReadMe. Added Config file of InstallForge. Handle csv locking of third party programs like Excel.

--- ArchiveViewer/ArchiveViewer/UserControls/Home/CsvImportExport.cs
+++ ArchiveViewer/ArchiveViewer/UserControls/Home/CsvImportExport.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Windows.Forms;
 using ArchiveViewer.Models;
 using Caliburn.Micro;
 using CsvHelper;
-using Microsoft.Win32;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
 
 namespace ArchiveViewer.UserControls.Home
 {
@@ -11,50 +15,66 @@ namespace ArchiveViewer.UserControls.Home
     {
         public void ImportCsv(BindableCollection<ArchiveItem> archiveItems)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "Csv documents (.csv)|*.csv";
-
-            bool? result = dlg.ShowDialog();
-            if (result == false)
+            try
             {
-                return;
-            }
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Csv documents (.csv)|*.csv";
 
-            using (TextReader reader = new StreamReader(dlg.FileName))
-            {
-                var csv = new CsvReader(reader);
-                csv.Configuration.HasHeaderRecord = true;
-                csv.Configuration.RegisterClassMap<ArchiveItemMap>();
-
-                archiveItems.Clear();
-                csv.ReadHeader();
-                while (csv.Read())
+                bool? result = dlg.ShowDialog();
+                if (result == false)
                 {
-                    var record = new ArchiveItem(csv[0], csv[1], csv[2], csv[3]);
-                    archiveItems.Add(record);
+                    return;
                 }
 
+                using (TextReader reader = new StreamReader(dlg.FileName))
+                {
+                    var csv = new CsvReader(reader);
+                    csv.Configuration.HasHeaderRecord = true;
+                    csv.Configuration.RegisterClassMap<ArchiveItemMap>();
+                    csv.Configuration.Encoding = Encoding.UTF8;
+
+                    archiveItems.Clear();
+                    csv.ReadHeader();
+                    while (csv.Read())
+                    {
+                        var record = new ArchiveItem(csv[0], csv[1], csv[2], csv[3]);
+                        archiveItems.Add(record);
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK);
             }
         }
 
         public void ExportCsv(Project project, IList<ArchiveItem> archiveItems, string selectedLanguage)
         {
-            SaveFileDialog dlg = new SaveFileDialog();
-            dlg.FileName = project.Name + "_" + selectedLanguage;
-            dlg.DefaultExt = ".csv";
-            dlg.Filter = "Csv documents (.csv)|*.csv";
-
-            bool? result = dlg.ShowDialog();
-            if (result == false)
+            try
             {
-                return;
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.FileName = project.Name + "_" + selectedLanguage;
+                dlg.DefaultExt = ".csv";
+                dlg.Filter = "Csv documents (.csv)|*.csv";
+
+                bool? result = dlg.ShowDialog();
+                if (result == false)
+                {
+                    return;
+                }
+
+                using (TextWriter writer = new StreamWriter(dlg.FileName))
+                {
+                    var csv = new CsvWriter(writer);
+                    csv.Configuration.RegisterClassMap<ArchiveItemMap>();
+                    csv.Configuration.Encoding = Encoding.UTF8;
+                    csv.WriteRecords(archiveItems);
+                }
             }
-
-            using (TextWriter writer = new StreamWriter(dlg.FileName))
+            catch (IOException e)
             {
-                var csv = new CsvWriter(writer);
-                csv.Configuration.RegisterClassMap<ArchiveItemMap>();
-                csv.WriteRecords(archiveItems);
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK);
             }
         }
     }
--- README.md
+++ README.md
@@ -11,11 +11,18 @@ Select the Game.manifest Unreal generates for you and bookmark it for later unde
 
 Select the language you want to translate and enter the translated text into the Translated column. After you are done press the Save Changes back to archive button.
 
-Aside from editing the translations in the Archive Viewer you can export and import them with the two lower left buttons.
+Aside from editing the translations in the Archive Viewer you can export and import them with the two lower left buttons. Translation data is exported in UTF-8.
 
 After you saved the changes press __Compile Text__ from your Localization Dashboard and the updated text should appear when you change the language and start the game in standalone mode.
 
 #### Automatic translation
-If you add a file named GoogleKey.json in the folder of the ArchivViewer.exe with a [Google Translation API](https://cloud.google.com/translate/docs/) Key inside, you can translate text automatically into the disired language with the buttons __Translate Selected__ and __Reverse translate selected__ to check if everything is properly translated in languages where you might otherwise not be able to read the letters.
 
-You still have to hire people to properly translate your game but it is good enough for testing.
+If you add a file named GoogleKey.json in the folder of the ArchivViewer.exe with a [Google Translation API](https://cloud.google.com/translate/docs/) Key inside, you can translate text automatically into the desired language with the buttons __Translate Selected__ and __Reverse translate selected__ to check if everything is properly translated in languages where you might otherwise not be able to read the letters.
+
+You still have to find people to properly translate your game but it is good enough for testing.
+
+#### Download
+
+Archive Viewer 1.0
+[Setup (Google Drive)](https://drive.google.com/open?id=10LkuDX48XDtfKPnD286QWKK7VzsAYj8r)
+[Portable (Google Drive)](https://drive.google.com/open?id=1CCg-2jrJvho9M6GMX1NBjpnac_qerdRe)
